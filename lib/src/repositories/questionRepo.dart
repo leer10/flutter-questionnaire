e9586fc5efc265567663dd781e84fbf4023270e0
Brandon Abbott <brandon.abbott@charter.net>
2019-09-27 21:09:11 -0700

    moved test questions to own file, now uses as backup

--- lib/src/repositories/questionRepo.dart
+++ lib/src/repositories/questionRepo.dart
@@ -1,12 +1,19 @@
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/widgets.dart';
 import 'package:questionairre/src/models/question.dart';
 
 class QuestionRepository {
   Questionairre questionairre;
 
   Future<void> loadQuestions() async {
-    String jsonString = await rootBundle.loadString('assets/questions.json');
+    String jsonString;
+    try {
+      jsonString = await rootBundle.loadString('assets/questions.json');
+    } on FlutterError {
+      print("Custom questions not found. loading example questions");
+      jsonString = await rootBundle.loadString('assets/examplequestions.json');
+    }
     questionairre = Questionairre.fromJson(jsonDecode(jsonString));
   }
 }
